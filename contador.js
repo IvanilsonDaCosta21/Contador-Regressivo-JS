ff282d3fcd4b3d4663a36fb2a52230f8584e61cb
Ivanilson dev <89939411+IvanilsonDaCosta21@users.noreply.github.com>
2021-10-21 18:07:11 -0100

    Add files via upload

---
+++ contador.js
@@ -0,0 +1,39 @@
+
+var dataFutura = new Date("November 12, 2022 10:00").getTime();
+
+var dias, horas, minutos, segundos;
+function setValores(e) {
+    return document.getElementById(e);
+    
+}
+
+// Executa uma funcao a cada 1segundo
+setInterval(function(){
+    var dataAtual = new Date().getTime();
+
+    var segundosTotal = (dataFutura - dataAtual)/1000;
+
+
+    dias = parseInt(segundosTotal/86400);
+    segundosTotal = segundosTotal%86400;
+
+    horas = parseInt(segundosTotal/3600);
+    segundosTotal = segundosTotal%3600;
+
+    minutos = parseInt(segundosTotal/60);
+
+    segundos = parseInt(segundosTotal%60);
+
+    setValores("dias").innerHTML = dias +" dias";
+    setValores("horas").innerHTML = horas +" horas";
+    setValores("minutos").innerHTML = minutos +" minutos";
+    setValores("segundos").innerHTML = segundos +" segundos";
+
+    
+
+    console.log(segundos);
+
+},1000);
+
+
+
